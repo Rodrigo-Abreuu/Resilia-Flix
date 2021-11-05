75d52470bf0b4a404957f6f084602c3ee5b36826
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2021-11-05 18:52:27 -0300

    Atualização da página login

--- login/login.js
+++ login/login.js
@@ -1,14 +1,17 @@
-function Login(event) {
-    event.preventDefault();
-    var usuario = document.getElementsByName('username')[0].value;
-    usuario = usuario.toLowerCase();
-    var senha = document.getElementsByName('password')[0].value;
-    senha = senha.toLowerCase();
+// function logar() {
+//     // event.preventDefault();
+//     var email = document.getElementsByName("email");
+//     // usuario = usuario.toLowerCase();
+//     var senha = document.getElementsByName("senha");
+//     // senha = senha.toLowerCase();
   
-    if (usuario == "bcf" && senha == "123") {
-      alert("dados corretos");
-      window.location = "index.html";
-    }else{
-      alert("Dados incorretos, tente novamente");
-    }
-  }
+//     if (email.value == "admin@admin.com" && senha.value == "admin") {
+//       // alert("dados corretos");
+//         localStorage.setItem("acesso", true);
+
+
+//       window.location.href = "index.html";
+//     }else{
+//       alert("Dados incorretos, tente novamente");
+//     }
+//   }
